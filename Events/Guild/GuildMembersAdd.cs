40d426f9976643e75bb2097b3ee3847779595021
Hisnuras <benjamin.macquet@outlook.fr>
2024-11-07 11:09:36 +0100

    GuildEvents
    Add file GuildMembersAdd.cs
    Add file GuildeMembersRemove.cs

---
+++ Events/Guild/GuildMembersAdd.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace HisnuBot.Events.Guild
+{
+	public class GuildMemberAdd
+	{
+		public static async Task OnGuildMemberAdded(SocketGuildUser user)
+		{
+			// Logique à exécuter lorsque quelqu'un rejoint le serveur
+			Console.WriteLine($"{user.Username} a rejoint le serveur !");
+		}
+	}
+}
---
+++ Events/Guild/GuildMembersRemove.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace HisnuBot.Events.Guild
+{
+	public class GuildMemberRemove
+	{
+		public static async Task OnGuildMemberRemoved(SocketGuildUser user)
+		{
+			// Logique à exécuter lorsque quelqu'un quitte le serveur
+			Console.WriteLine($"{user.Username} a quitté le serveur !");
+		}
+	}
+}
